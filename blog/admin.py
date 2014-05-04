f024f5b626229e895784d1fe6e1c0e238b1255a6
K-DOT <aleximeky@gmail.com>
2014-05-04 20:58:22 +0400

    admin.py edited, added requirements.txt

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 from django.contrib import admin
 from blog.models import Post, Category # наша модель из books/models.py
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'datetime', 'category', 'image')
+    list_filter = ('datetime',)
+    search_fields = ('title',)
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
